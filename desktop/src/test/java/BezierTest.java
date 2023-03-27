6900072adf9e7a6901479ebcf0c1a5bad135b43a
acm022 <michael.babic@haw-hamburg.de>
2023-03-27 12:51:32 +0200

    finish tests

--- desktop/src/test/java/BezierTest.java
+++ desktop/src/test/java/BezierTest.java
@@ -26,9 +26,9 @@ public class BezierTest {
         point05 = new Vector2f(0.5f, 0.25f);
         point1 = new Vector2f(1, 0);
         // vectors for derivative
-        derivative1 = new Vector2f(0.5f, 0.5f);
-        derivative2 = new Vector2f(1.5f, 0.5f);
-        derivative3 = new Vector2f(0.5f, 0.5f);
+        derivative1 = new Vector2f(1.0f, 1.0f);
+        derivative2 = new Vector2f(1.0f, 0.0f);
+        derivative3 = new Vector2f(-1.0f, -1.0f);
 
     }
 
@@ -86,19 +86,19 @@ public class BezierTest {
         float t;
         t = 0.0f;
         Vector2f vector1 = curve.evalDerivative(t);
-        // assertEquals(derivative1, vector1);
+        assertEquals(derivative1, vector1);
         System.out.println("vector1");
         System.out.println(vector1);
 
         t = 0.5f;
         Vector2f vector2 = curve.evalDerivative(t);
-        // assertEquals(derivative2, vector2);
+        assertEquals(derivative2, vector2);
         System.out.println("vector2");
         System.out.println(vector2);
         t = 1.0f;
 
         Vector2f vector3 = curve.evalDerivative(t);
-        // assertEquals(derivative3, vector3);
+        assertEquals(derivative3, vector3);
         System.out.println("vector3");
         System.out.println(vector3);
 
